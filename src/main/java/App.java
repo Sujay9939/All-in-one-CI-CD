fe45d8101e831aa942747b22749e3bccfd664424
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-03-28 21:56:19 +0530

    Update output message to include Java version

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD 🚀");
+        System.out.println("Hello from Jenkins CI/CD 🚀 Java 21");
     }
 }
